145f150b85499785baf18c89bf9e7486686c2807
TheKit <nekit1000@gmail.com>
2023-09-17 22:12:16 +0300

    setup_repositories.sh: always use dynparts ramdisk
    After Android 10, dynamic partitions became the default and
    we need dynparts ramdisk even if the rootfs is installed in userdata
    (useful during early porting stage).
    
    Use the dynparts ramdisk by default. If porting for a legacy
    device with small boot.img size, the old "default" ramdisk can be
    still selected by setting deviceinfo_prebuilt_boot_ramdisk_source.

--- setup_repositories.sh
+++ setup_repositories.sh
@@ -183,12 +183,9 @@ setup_ramdisk() {
     elif [ -n "$deviceinfo_prebuilt_boot_ramdisk_source" ]; then
         print_message "Downloading prebuilt ramdisk from: $deviceinfo_prebuilt_boot_ramdisk_source"
         RAMDISK_URL="$deviceinfo_prebuilt_boot_ramdisk_source"
-    elif [[ "$deviceinfo_kernel_cmdline" = *"systempart=/dev/mapper"* ]]; then
-        print_message "Selecting dynparts ramdisk for devices with dynamic partitions"
-        RAMDISK_URL="https://github.com/halium/initramfs-tools-halium/releases/download/dynparts/initrd.img-touch-${RAMDISK_ARCH}"
     else
-        print_message "Selecting default ramdisk"
-        RAMDISK_URL="https://github.com/halium/initramfs-tools-halium/releases/download/continuous/initrd.img-touch-${RAMDISK_ARCH}"
+        print_message "Downloading dynparts ramdisk for devices with dynamic partitions"
+        RAMDISK_URL="https://github.com/halium/initramfs-tools-halium/releases/download/dynparts/initrd.img-touch-${RAMDISK_ARCH}"
     fi
     curl --location --output halium-boot-ramdisk.img "$RAMDISK_URL"
 }
